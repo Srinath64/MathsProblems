f10fb10b4fc7241eb464efc20b5ca4229f5b7925
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-04-27 14:29:05 +0530

    Added : Addition Program

--- src/com/bridgelabz/mathsproblems/Maths.java
+++ src/com/bridgelabz/mathsproblems/Maths.java
@@ -3,5 +3,9 @@ package com.bridgelabz.mathsproblems;
 public class Maths {
     public static void main(String[] args) {
         System.out.println("Welcome to Maths Program");
+
+        int a=10 , b=20 , c ;
+        c=a+b;
+        System.out.println("The Addition is :" +c);
     }
 }
