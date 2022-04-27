23f4d55e3bf1cd75380bfa3975a62b553363f52b
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-04-27 14:33:32 +0530

    Added : Subraction Program

--- src/com/bridgelabz/mathsproblems/Maths.java
+++ src/com/bridgelabz/mathsproblems/Maths.java
@@ -4,8 +4,10 @@ public class Maths {
     public static void main(String[] args) {
         System.out.println("Welcome to Maths Program");
 
-        int a=10 , b=20 , c ;
+        int a=10 , b=20 , c ,d;
         c=a+b;
+        d = a-b;
         System.out.println("The Addition is :" +c);
+        System.out.println("The Subraction is :" +d);
     }
 }
